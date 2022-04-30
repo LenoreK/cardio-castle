8a9ff21954ebfcf4756ea69700cca681bcf75057
Lenore Kelly <lenorekelly2@gmail.com>
2022-04-30 19:11:29 -0400

    working on sectioning for profile

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 function Profile() {
     return (
-        <div>
-            <h1>Hey! It's me! read all about it</h1>
-        </div>
+        <body>
+            <div class='main-container'>
+                <header>
+                    <h1>Cardio Castle</h1>
+                </header>
+                <aside>
+                    <nav>
+                        Aside / Nav
+                    </nav>
+                </aside>
+            <main>
+                <div class='main-image'>Main Image</div>
+                <div class='two-row'>Row 2</div>
+                <div class='three-row'>Row 3</div>
+            </main>
+        <footer>
+            Footer
+        </footer>
+    </div>
+</body>
     )
 }
 
