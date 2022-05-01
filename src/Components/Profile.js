0a1d7b4e0c14ac9386c0566b228c4be4890f42d2
Lenore Kelly <lenorekelly2@gmail.com>
2022-05-01 11:12:43 -0400

    added tabs

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Aside from './Aside.js'
 import TopBar from './TopBar.js'
+import { Tabs } from 'react-bootstrap';
+import { Tab } from 'react-bootstrap';
 
 function Profile() {
     return (
@@ -10,12 +12,13 @@ function Profile() {
                 <Aside />
                 <main>
                     <div className='one-row'>
-                        <div>Week 1</div>
-                        <div>Week 2</div>
-                        <div>Week 3</div>
-                        <div>Week 4</div>
+                        <Tabs defaultActiveKey="first">
+                            <Tab eventKey="first" title="Week 1">Week 1</Tab>
+                            <Tab eventKey="second" title="Week 2">Week 2</Tab>
+                            <Tab eventKey="third" title="Week 3">Week 3</Tab>
+                            <Tab eventKey="fourth" title="Week 4">Week 4</Tab>
+                        </Tabs>
                     </div>
-                    <div className='two-row'>Row 2</div>
                     <div className='three-row'>Row 3</div>
                 </main>
         <footer>
