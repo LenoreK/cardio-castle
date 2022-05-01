28f7178815f1218d9195ad87d4b0d81d395e5766
Lenore Kelly <lenorekelly2@gmail.com>
2022-05-01 13:36:58 -0400

    I've added the days into the website

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -15,23 +15,103 @@ function Profile() {
                         <Tabs defaultActiveKey="first">
                             <Tab eventKey="first" title="Week 1" className="week-1">
                                 <div className='daily'>
-                                    <Container fluid className="a">a</Container>
-                                    <Container fluid className="b">b</Container>
-                                    <Container fluid className="c">c</Container>
-                                    <Container fluid className="d">d</Container>
-                                    <Container fluid className="e">e</Container>
-                                    <Container fluid className="f">f</Container>
-                                    <Container fluid className="g">g</Container>
+                                    <Container fluid className="a">
+                                        <h2>Day 1</h2>
+                                    </Container>
+                                    <Container fluid className="b">
+                                        <h2>Day 2</h2>
+                                    </Container>
+                                    <Container fluid className="c">
+                                        <h2>Day 3</h2>
+                                    </Container>
+                                    <Container fluid className="d">
+                                        <h2>Day 4</h2>
+                                    </Container>
+                                    <Container fluid className="e">
+                                        <h2>Day 5</h2>
+                                    </Container>
+                                    <Container fluid className="f">
+                                        <h2>Day 6</h2>
+                                    </Container>
+                                    <Container fluid className="g">
+                                        <h2>Day 7</h2>
+                                    </Container>
                                 </div>
                             </Tab>
                             <Tab eventKey="second" title="Week 2">
-                                
+                                <div className='daily'>
+                                    <Container fluid className="a">
+                                        <h2>Day 1</h2>
+                                    </Container>
+                                    <Container fluid className="b">
+                                        <h2>Day 2</h2>
+                                    </Container>
+                                    <Container fluid className="c">
+                                        <h2>Day 3</h2>
+                                    </Container>
+                                    <Container fluid className="d">
+                                        <h2>Day 4</h2>
+                                    </Container>
+                                    <Container fluid className="e">
+                                        <h2>Day 5</h2>
+                                    </Container>
+                                    <Container fluid className="f">
+                                        <h2>Day 6</h2>
+                                    </Container>
+                                    <Container fluid className="g">
+                                        <h2>Day 7</h2>
+                                    </Container>
+                                </div>
                             </Tab>
                             <Tab eventKey="third" title="Week 3">
-                                
+                                <div className='daily'>
+                                    <Container fluid className="a">
+                                        <h2>Day 1</h2>
+                                    </Container>
+                                    <Container fluid className="b">
+                                        <h2>Day 2</h2>
+                                    </Container>
+                                    <Container fluid className="c">
+                                        <h2>Day 3</h2>
+                                    </Container>
+                                    <Container fluid className="d">
+                                        <h2>Day 4</h2>
+                                    </Container>
+                                    <Container fluid className="e">
+                                        <h2>Day 5</h2>
+                                    </Container>
+                                    <Container fluid className="f">
+                                        <h2>Day 6</h2>
+                                    </Container>
+                                    <Container fluid className="g">
+                                        <h2>Day 7</h2>
+                                    </Container>
+                                </div>
                             </Tab>
                             <Tab eventKey="fourth" title="Week 4">
-                                
+                                <div className='daily'>
+                                    <Container fluid className="a">
+                                        <h2>Day 1</h2>
+                                    </Container>
+                                    <Container fluid className="b">
+                                        <h2>Day 2</h2>
+                                    </Container>
+                                    <Container fluid className="c">
+                                        <h2>Day 3</h2>
+                                    </Container>
+                                    <Container fluid className="d">
+                                        <h2>Day 4</h2>
+                                    </Container>
+                                    <Container fluid className="e">
+                                        <h2>Day 5</h2>
+                                    </Container>
+                                    <Container fluid className="f">
+                                        <h2>Day 6</h2>
+                                    </Container>
+                                    <Container fluid className="g">
+                                        <h2>Day 7</h2>
+                                    </Container>
+                                </div>
                             </Tab>
                         </Tabs>
                     </div>
