ddd62a9da073e9a328b6118b616e0d59fa3c3474
Lenore Kelly <lenorekelly2@gmail.com>
2022-05-03 17:58:51 -0400

    added buttons

--- src/Components/WeeklyRecords.js
+++ src/Components/WeeklyRecords.js
@@ -11,25 +11,39 @@ function WeeklyRecord() {
                             <Tab eventKey="first" title="Week 1" className="week-1">
                                 <div className='daily'>
                                     <Container fluid className="a">
+                                    <button type='button' className='button'>
                                         <h2>Day 1</h2>
+                                    </button>
                                     </Container>
                                     <Container fluid className="b">
-                                        <h2>Day 2</h2>
+                                        <button type='button' className='button'>
+                                            <h2>Day 2</h2>
+                                        </button>
                                     </Container>
                                     <Container fluid className="c">
-                                        <h2>Day 3</h2>
+                                        <button type='button' className='button'>
+                                            <h2>Day 3</h2>
+                                        </button>
                                     </Container>
                                     <Container fluid className="d">
-                                        <h2>Day 4</h2>
+                                        <button type='button' className='button'>
+                                            <h2>Day 4</h2>
+                                        </button>
                                     </Container>
                                     <Container fluid className="e">
-                                        <h2>Day 5</h2>
+                                        <button type='button' className='button'>
+                                            <h2>Day 5</h2>
+                                        </button>
                                     </Container>
                                     <Container fluid className="f">
-                                        <h2>Day 6</h2>
+                                        <button type='button' className='button'>
+                                            <h2>Day 6</h2>
+                                        </button>
                                     </Container>
                                     <Container fluid className="g">
-                                        <h2>Day 7</h2>
+                                        <button type='button' className='button'>
+                                            <h2>Day 7</h2>
+                                        </button>
                                     </Container>
                                 </div>
                             </Tab>
