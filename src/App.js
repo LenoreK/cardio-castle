b87367b2ccb131957e3343e4a8e9a934bce0e154
Araz Shahinian <arshahinian@gmail.com>
2022-05-05 19:49:47 -0400

    You can add a Goal :)

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App(props) {
 	let [message, setMessage] = useState('Search for Music!')
 	let [userData, setUserData] = useState({})
   let [goalData, setGoalData] = useState('')
+  let [isNewGoal, setIsNewGoal] = useState(false)
 
   const API_URL = 'https://cardio-castle-foundation.herokuapp.com/users/'
 
@@ -75,31 +76,98 @@ function App(props) {
 		}
 	}
 
-  const handleGoalData = (e, goalData) => {
+  const handleGoalData = (e, goalName) => {
+    let userID = userData.id
     const API_Goal = 'https://cardio-castle-foundation.herokuapp.com/goals/'
 		e.preventDefault()
-    console.log(goalData)
-		setGoalData(goalData)
-    let userID = userData.id
-    if(goalData) {
-			const fetchData = async () => {
-        let fetchString = API_Goal + goalData + "?userId=" + userID
+    if(goalName) {
+      console.log(' GOAL NAME IS POPULATED ')
+      /* GET GOALS AND SEE IF THE USER ALEADY HAS A GOAL */
+      const fetchAllGoalData = async () => {
+        let fetchString = API_Goal
         let response = await fetch(fetchString,{
-					crossDomain:true,
-					method: 'GET',
-					headers: {'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}})
-				let resData = await response.json();
-				if (resData.id != null) {
-					setGoalData(resData)
-          console.log(resData)
-				} else {
-					setMessage('Not Found')
-				}
-			}
-			fetchData()
-    }
-	}
+        crossDomain:true,
+        method: 'GET',
+        headers: {'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}})
+        let resData = await response.json();
 
+        let userHasGoal = false;
+
+        for(let i = 0;i<resData.length;i++)
+        {
+          console.log(resData[i])
+          if(resData[i] != null && resData[i].id != null)
+          {
+             if(resData[i].user_account_id == userID && resData[i].goal_status == 1)
+             {
+                userHasGoal = true;
+             }
+          }
+        }
+
+        if (userHasGoal) {
+          console.log("You already have a goal finish it!")
+          setMessage("You already have a goal finish it!")
+        } else {    
+        
+          /* GET GOAL */
+          console.log(' GET GOAL ')
+          console.log(goalName)
+          
+          const fetchExistingGoalData = async () => {
+            let fetchString = API_Goal + goalName + "?userId=" + userID
+            let response = await fetch(fetchString,{
+            crossDomain:true,
+            method: 'GET',
+            headers: {'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}})
+            let resData = await response.json();
+            if (resData != null && resData.id != null) {
+              console.log(' GOAL NAME EXISTS ')
+              setGoalData(resData)
+              console.log(resData)
+            } else {
+              console.log(' GOAL NAME NEW ')
+              setIsNewGoal(true);
+              console.log(' POST GOAL ')
+              console.log(goalName)
+              console.log(`handleNewGoal - ${goalName}`)
+              /* POST / CREATE GOAL */
+              let fetchNewGoalData = async () => {
+              let fetchString = API_Goal
+              console.log(fetchString)				
+              let response = await fetch(fetchString,{
+                crossDomain:true,
+                method: 'POST',
+                headers: {'Content-Type':'application/json','Access-Control-Allow-Origin':'*','Access-Control-Allow-Methods': 'POST'},          
+                body: JSON.stringify({"goal_name":goalName,"goal_status":1,"last_modified":"05/01/2022","last_modified_by":"Website","user_account_id":userID})
+                })
+                console.log(response)
+                setMessage(response)
+                const fetchNewGoalDataInfo = async () => {
+                  let fetchString = API_Goal + goalName + "?userId=" + userID
+                  let response = await fetch(fetchString,{
+                    crossDomain:true,
+                    method: 'GET',
+                    headers: {'Content-Type':'application/json','Access-Control-Allow-Origin':'*'}})
+                  let resData = await response.json();
+                  if (resData != null && resData.id != null) {
+                    setGoalData(resData)
+                    console.log(resData)
+                  } else {
+                    setMessage('No Goal Found')
+                  }
+                }
+                fetchNewGoalDataInfo()
+              }
+              fetchNewGoalData()	
+            }
+          }
+          fetchExistingGoalData()            
+        }
+      }
+      fetchAllGoalData();      
+    }   
+  }
   // let tbd = JSON.stringify(userData)
 
   // const adminUser = {
